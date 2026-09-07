33e86361fc309ad40e6850c676e157545bc37ddb
Davi Garrocho da Silva <davigarrocho@proton.me>
2026-09-07 11:41:37 -0300

    consertei o dash que tava dando uns bug

--- scenes/plyer/player.gd
+++ scenes/plyer/player.gd
@@ -4,9 +4,12 @@ extends CharacterBody2D
 @export var dash_speed := 250.0
 @export var dash_duration := 0.15
 @export var estamina := 300
-@onready var dash_timer: Timer = $dash_timer
+@onready var estamina_timer: Timer = $estamina_timer
+@onready var dash_timer: Timer = $dashtimer
 @onready var anim_tree: AnimationTree = $AnimationTree
 @onready var anim_state = anim_tree.get("parameters/playback")
+@onready var label: Label = $Label
+
 
 var current_state = player_state.MOVE
 var last_direction := Vector2.DOWN
@@ -17,9 +20,11 @@ enum player_state {
 }
 
 func _ready() -> void:
-	dash_timer.start()
+	estamina_timer.start()
+	dash_timer.wait_time = dash_duration
 func _physics_process(_delta):
-	print(estamina)
+	label.text = str(estamina)
+	
 	match current_state:
 			
 		player_state.MOVE:
@@ -47,20 +52,19 @@ func move():
 		velocity = Vector2.ZERO
 	#dash
 	if Input.is_action_just_pressed("dash") and estamina >= 100:
+		estamina_timer.start()
 		estamina -= 100
-		dash_timer.start()
 		current_state = player_state.DASH
-		velocity = last_direction * dash_speed
+		dash_timer.start()
 		return
 	move_and_slide()
 
 
 func dash():
-		velocity = last_direction * dash_speed
-		move_and_slide()
-		await get_tree().create_timer(dash_duration).timeout
-		current_state = player_state.MOVE
-
+	velocity = last_direction * dash_speed
+	move_and_slide()
+func _on_dashtimer_timeout() -> void:
+	current_state = player_state.MOVE
 
 func animation_state(direction):
 	anim_tree.set("parameters/idle/blend_position", direction)
